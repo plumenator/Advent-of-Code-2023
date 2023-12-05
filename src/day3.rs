5e52eb052ce30374cdd2b4d962d530e851f7d132
plumenator <karthik.ravikanti@gmail.com>
2023-12-05 15:46:18 +0000

    Solve day 3 part 1

--- src/day3.rs
+++ src/day3.rs
@@ -1,18 +1,20 @@
 /*
---- Day 3: Gear Ratios ---
-You and the Elf eventually reach a gondola lift station; he says the gondola lift will take you up to the water source, but this is as far as he can bring you. You go inside.
+our puzzle answer was 538046.
 
-It doesn't take long to find the gondolas, but there seems to be a problem: they're not moving.
+The first half of this puzzle is complete! It provides one gold star: *
 
-"Aaah!"
+--- Part Two ---
+The engineer finds the missing part and installs it in the engine! As the engine springs to life, you jump in the closest gondola, finally ready to ascend to the water source.
 
-You turn around to see a slightly-greasy Elf with a wrench and a look of surprise. "Sorry, I wasn't expecting anyone! The gondola lift isn't working right now; it'll still be a while before I can fix it." You offer to help.
+You don't seem to be going very fast, though. Maybe something is still wrong? Fortunately, the gondola has a phone labeled "help", so you pick it up and the engineer answers.
 
-The engineer explains that an engine part seems to be missing from the engine, but nobody can figure out which one. If you can add up all the part numbers in the engine schematic, it should be easy to work out which part is missing.
+Before you can explain the situation, she suggests that you look out the window. There stands the engineer, holding a phone in one hand and waving with the other. You're going so slowly that you haven't even left the station. You exit the gondola.
 
-The engine schematic (your puzzle input) consists of a visual representation of the engine. There are lots of numbers and symbols you don't really understand, but apparently any number adjacent to a symbol, even diagonally, is a "part number" and should be included in your sum. (Periods (.) do not count as a symbol.)
+The missing part wasn't the only issue - one of the gears in the engine is wrong. A gear is any * symbol that is adjacent to exactly two part numbers. Its gear ratio is the result of multiplying those two numbers together.
 
-Here is an example engine schematic:
+This time, you need to find the gear ratio of every gear and add them all up so that the engineer can figure out which gear needs to be replaced.
+
+Consider the same engine schematic again:
 
 467..114..
 ...*......
@@ -24,9 +26,9 @@ Here is an example engine schematic:
 ......755.
 ...$.*....
 .664.598..
-In this schematic, two numbers are not part numbers because they are not adjacent to a symbol: 114 (top right) and 58 (middle right). Every other number is adjacent to a symbol and so is a part number; their sum is 4361.
+In this schematic, there are two gears. The first is in the top left; it has part numbers 467 and 35, so its gear ratio is 16345. The second gear is in the lower right; its gear ratio is 451490. (The * adjacent to 617 is not a gear because it is only adjacent to one part number.) Adding up all of the gear ratios produces 467835.
 
-Of course, the actual engine schematic is much larger. What is the sum of all of the part numbers in the engine schematic?
+What is the sum of all of the gear ratios in your engine schematic?
 
 */
 
@@ -52,10 +54,13 @@ struct Symbol {
 impl Number {
     fn part(&self, symbols: &Vec<Symbol>, width: usize) -> bool {
         symbols.iter().any(|s| {
-            //eprintln!("{:?}, {:?}", s, self);
-            (s.index >= self.start - 1 && s.index <= self.end + 1)
+            let yes = (s.index >= self.start - 1 && s.index <= self.end + 1)
                 || (s.index - width >= self.start - 1 && s.index - width <= self.end + 1)
-                || (s.index + width >= self.start - 1 && s.index + width <= self.end + 1)
+                || (s.index + width >= self.start - 1 && s.index + width <= self.end + 1);
+            if yes {
+                eprintln!("{:?}, {:?}", s, self);
+            }
+            yes
         })
     }
 }
@@ -66,7 +71,8 @@ pub fn part1(file_name: &str) -> u64 {
     let (numbers, symbols) = parse_schematic(&input);
     numbers
         .iter()
-        .map(|g| if g.part(&symbols, width) { g.value } else { 0 })
+        .filter(|n| n.part(&symbols, width))
+        .map(|n| n.value)
         .sum()
 }
 
@@ -98,7 +104,7 @@ fn parse_schematic(input: &str) -> (Vec<Number>, Vec<Symbol>) {
                 numbers.push(Number {
                     value,
                     start: curr,
-                    end: curr + orig_len - remaining.len(),
+                    end: curr + orig_len - remaining.len() - 1,
                 });
             }
             Token::Sym => {
@@ -123,6 +129,6 @@ mod test {
     }
     #[test]
     fn part1_actual() {
-        assert_eq!(super::part1("src/day3_input.txt"), 4361)
+        assert_eq!(super::part1("src/day3_input.txt"), 538046)
     }
 }
