46ae6d4e3046ecb4129904cc3791792728da712a
plumenator <karthik.ravikanti@gmail.com>
2023-12-05 08:45:39 +0000

    Solved day 2 part 2

--- src/day2.rs
+++ src/day2.rs
@@ -24,6 +24,34 @@ The Elf would first like to know which games would have been possible if the bag
 In the example above, games 1, 2, and 5 would have been possible if the bag had been loaded with that configuration. However, game 3 would have been impossible because at one point the Elf showed you 20 red cubes at once; similarly, game 4 would also have been impossible because the Elf showed you 15 blue cubes at once. If you add up the IDs of the games that would have been possible, you get 8.
 
 Determine which games would have been possible if the bag had been loaded with only 12 red cubes, 13 green cubes, and 14 blue cubes. What is the sum of the IDs of those games?
+
+Your puzzle answer was 2541.
+
+The first half of this puzzle is complete! It provides one gold star: *
+
+--- Part Two ---
+The Elf says they've stopped producing snow because they aren't getting any water! He isn't sure why the water stopped; however, he can show you how to get to the water source to check it out for yourself. It's just up ahead!
+
+As you continue your walk, the Elf poses a second question: in each game you played, what is the fewest number of cubes of each color that could have been in the bag to make the game possible?
+
+Again consider the example games from earlier:
+
+Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+In game 1, the game could have been played with as few as 4 red, 2 green, and 6 blue cubes. If any color had even one fewer cube, the game would have been impossible.
+Game 2 could have been played with a minimum of 1 red, 3 green, and 4 blue cubes.
+Game 3 must have been played with at least 20 red, 13 green, and 6 blue cubes.
+Game 4 required at least 14 red, 3 green, and 15 blue cubes.
+Game 5 needed no fewer than 6 red, 3 green, and 2 blue cubes in the bag.
+The power of a set of cubes is equal to the numbers of red, green, and blue cubes multiplied together. The power of the minimum set of cubes in game 1 is 48. In games 2-5 it was 12, 1560, 630, and 36, respectively. Adding up these five powers produces the sum 2286.
+
+For each game, find the minimum set of cubes that must have been present. What is the sum of the power of these sets?
+
+Your puzzle answer was 66016.
+
 */
 
 use std::fs::read_to_string;
@@ -54,6 +82,12 @@ impl Game {
             .iter()
             .all(|s| s.red <= 12 && s.green <= 13 && s.blue <= 14)
     }
+
+    fn power(&self) -> u64 {
+        self.sets.iter().map(|s| s.red).max().unwrap_or_default()
+            * self.sets.iter().map(|s| s.green).max().unwrap_or_default()
+            * self.sets.iter().map(|s| s.blue).max().unwrap_or_default()
+    }
 }
 
 pub fn part1(file_name: &str) -> u64 {
@@ -66,6 +100,15 @@ pub fn part1(file_name: &str) -> u64 {
         .iter()
         .map(|g| if g.possible() { g.id } else { 0 })
         .sum()
+}
+
+pub fn part2(file_name: &str) -> u64 {
+    let games: Vec<_> = read_to_string(file_name)
+        .unwrap()
+        .split('\n')
+        .map(parse_game)
+        .collect();
+    games.iter().map(|g| g.power()).sum()
 }
 
 fn parse_game(input: &str) -> Game {
@@ -108,9 +151,14 @@ fn count(counts: &Vec<(u64, &str)>, color: &str) -> u64 {
 
 #[cfg(test)]
 mod test {
-    #[test]
-    fn part1() {
-        assert_eq!(super::part1("src/day2_test_input.txt"), 8);
-        assert_eq!(super::part1("src/day2_input.txt"), 2541)
-    }
+#[test]
+fn part1() {
+    assert_eq!(super::part1("src/day2_test_input.txt"), 8);
+    assert_eq!(super::part1("src/day2_input.txt"), 2541)
+}
+#[test]
+fn part2() {
+    assert_eq!(super::part2("src/day2_test_input.txt"), 2286);
+    assert_eq!(super::part2("src/day2_input.txt"), 66016)
+}
 }
